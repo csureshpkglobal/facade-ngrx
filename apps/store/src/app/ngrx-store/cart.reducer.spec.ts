233d6c62974eef54c5b987e85134189e2f303731
Suresh Chitrotu <csuresh@pkglobal.com>
2021-06-04 11:57:47 +0530

    Updated with test cases

--- apps/store/src/app/ngrx-store/cart.reducer.spec.ts
+++ apps/store/src/app/ngrx-store/cart.reducer.spec.ts
@@ -1,41 +1,66 @@
-import { Action } from '@ngrx/store';
+import { Action, State } from '@ngrx/store';
 import * as CartActions from './cart.actions';
 import * as cartReducer from '../ngrx-store/cart.reducer';
+import { Book } from '../book.model';
+import { Collection } from '../collection.model';
 
 describe('CartReducer', () => {
+  let initialState: cartReducer.State;
+  let book: Book;
+  let collection: Collection;
+  beforeEach(() => {
+    initialState = cartReducer.initialState;
+    book = {
+      id: '1',
+      title: 'title',
+      imageLink: '',
+      description: '',
+      authors: '',
+      ratingsCount: '',
+      publisher: '',
+      language: '',
+      pageCount: '',
+    };
+    collection = {
+      title: '',
+      imgLink: '',
+      description: '',
+      authors: '',
+      name: '',
+      email: '',
+      phone: 0,
+      address: '',
+    };
+  });
+  afterEach(() => {
+    initialState = null;
+  });
+
   it('should create cart reducer', () => {
     expect(cartReducer.cartReducer).toBeTruthy();
   });
 
-  it('should return initial state', () => {
-    const { initialState } = cartReducer;
-    // const MOCK_ACTION = '[MOCK] ACTION';
+  it('should return the default state', () => {
+    const state = cartReducer.cartReducer(undefined, { type: null });
 
-    // class MockAction implements Action {
-    //   readonly type = MOCK_ACTION;
-    //   readonly payload: unknown;
-
-    //   constructor(payload?: unknown) {
-    //     this.payload = payload;
-    //   }
-    // }
-
-    // function createMockReducer<T>(initialState: T) {
-    //     return function reducer(state: any = initialState, action: { payload: any }): T {
-    //       // Everything will be stored to the store
-    //       return { ...state, ...action.payload };
-    //     };
-    //   }
-
-    const action = {
-      type: 'Unknown',
-    };
-    const state = cartReducer.cartReducer(initialState, action);
-    // const noopAction = new Action('noop' as CartActions);
-    // const newState = todoListReducers(undefined, noopAction);
     expect(state).toBe(initialState);
   });
-  it('should', () => {
-    expect(1).toBeTruthy();
+
+  it('should add book to store CartItems', () => {
+    const action = new CartActions.AddCartItem(book);
+    const state = cartReducer.cartReducer(initialState, action);
+
+    expect(state.cartItems.length).toEqual(1);
+    expect(state.cartItems[0]).toEqual(book);
+  });
+  it('should delete book from store CartItems', () => {
+    const addAction = new CartActions.AddCartItem(book);
+    const addState = cartReducer.cartReducer(initialState, addAction);
+    expect(addState.cartItems.length).toEqual(1);
+
+    const deleteAction = new CartActions.DeletCartItem('1');
+    const state = cartReducer.cartReducer(addState, deleteAction);
+
+    expect(state.cartItems.length).toEqual(0);
   });
 });
